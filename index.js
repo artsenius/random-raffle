2ca7368a83abedcae74cbdc59a54481508a91c51
Arthur Senko <asenko@alluma.org>
2020-12-12 20:49:52 -0800

    final commit

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 function count() {
-    let initialDate = new Date(document.getElementById('date-value').value);
+    let initialDate = document.getElementById('date-value').value;
     if (initialDate.length < 1) {
         alert('Please specify initial date')
     } else {
+        let initialDateMs = new Date(initialDate);
         let addDays = document.getElementById('add-value').value;
         if (addDays.length < 1) {
             alert('Please specify number of days to add')
         } else {
-            let futureDateMs = initialDate.getTime() + (parseInt(addDays) + 1) * 86400000;
+            let futureDateMs = initialDateMs.getTime() + (parseInt(addDays) + 1) * 86400000;
             let futureDate = new Date(futureDateMs).toLocaleDateString()
             document.getElementById('new-value').innerHTML = futureDate;
         }
