6a10c6a57be44502e8013e0f2dc2a328b2054910
Arthur Senko <asenko@alluma.org>
2021-01-11 17:40:06 -0800

    raffle

--- index.js
+++ index.js
@@ -1,16 +1,56 @@
-function count() {
-    let initialDate = document.getElementById('date-value').value;
-    if (initialDate.length < 1) {
-        alert('Please specify initial date')
+function validation() {
+    const noOfWinners = document.getElementById('no-of-winners').value;
+    const participants = document.getElementById('participants').value;
+    const partArray = participants.split(',');
+
+    if (noOfWinners.trim().length === 0) {
+        alert('Please specify the number of winners');
+    } else if (parseInt(noOfWinners) === 0) {
+        alert('Number of winners should be between 1 and 999');
+    } else if (participants.trim().length === 0) {
+        alert('Please specify the participants');
+    } else if (parseInt(noOfWinners) >= partArray.length) {
+        alert('Number of winners shoulbe be greater than number of participants');
     } else {
-        let initialDateMs = new Date(initialDate);
-        let addDays = document.getElementById('add-value').value;
-        if (addDays.length < 1) {
-            alert('Please specify number of days to add')
+        raffle();
+    }
+}
+
+function raffle() {
+    const noOfWinners = document.getElementById('no-of-winners').value;
+    const participants = document.getElementById('participants').value;
+    const partArray = participants.split(',');
+    const winners = [];
+
+    for (let i = 0; i < 9999999; i++) {
+        let winner = partArray[Math.floor(Math.random() * partArray.length)];
+        if (winners.includes(winner)) {
+            continue;
+        } else if (winners.includes(winner) === false && winners.length < parseInt(noOfWinners)) {
+            winners.push(winner);
         } else {
-            let futureDateMs = initialDateMs.getTime() + (parseInt(addDays) + 1) * 86400000;
-            let futureDate = new Date(futureDateMs).toLocaleDateString()
-            document.getElementById('new-value').innerHTML = futureDate;
+            break;
         }
     }
+
+    console.log(winners);
+
+    document.getElementById('raffle-holder').innerHTML =
+        `<div class="card-holder">
+            <div class="card-body">
+                <div class="text-center card-title mb-2" id="winners-title">Winners:</div>
+                <div>
+                    ${winners.map(el => `<div>${el}</div>`).join('')}
+                </div>
+            </div>
+        </div>
+        
+        <div class="text-center mt-3">
+            <button onclick="reload()">New Raffle</button>
+        </div
+        `;
+}
+
+function reload() {
+    location.reload();
 }
